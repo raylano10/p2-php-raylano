250c805daabc745d86d21eb232e0c0cd3ea194ca
raylano10 <143798089+raylano10@users.noreply.github.com>
2023-12-19 14:09:28 +0100

    new

---
+++ opdr-h2-helloworld/helloworld.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World example</title>
+</head>
+<body>
+    
+    <h1>Raylano</h1>
+    <?php
+    echo "hello <b>world</b>";
+    ?>
+</body>
+</html>
